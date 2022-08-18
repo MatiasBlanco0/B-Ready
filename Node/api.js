14df1cf355564986b00dd8cd5dd37e9be25d236b
MatiasBlanco0 <101400282+MatiasBlanco0@users.noreply.github.com>
2022-08-18 15:11:24 -0300

    Update api.js

--- Node/api.js
+++ Node/api.js
@@ -16,37 +16,49 @@ function closePool(){
 
 async function sqlQuery (query, values){
     console.log("Running query");
-    let promise = new Promise((resolve) => {
+    return await new Promise((resolve) => {
         pool.query(query, values, (err, result) => {
+            // Solo esta para el testeo de la funcion, sino queda el programa corriendo
             closePool();
+
             if(err) throw err;
             resolve(result);
         });
     });
-
-    return await promise;
 }
 
 async function nameLogIn(name, password){
     let value = "No se usar promesas";
     let sql = "SELECT 1 FROM usuario WHERE usuario.nombre = ? AND usuario.contrasenia = ?";
-    let values = [name, password];
-    let promise = await sqlQuery(sql, values);
-    console.log("------------------------------\nInside nameLogIn: ")
+    let promise = await sqlQuery(sql, [name, password]);
+
+    console.log("------------------------------");
+    console.log("Inside nameLogIn: ");
     console.log("Promise:");
     console.table(promise);
+
     value = promise.length > 0;
     console.log("Value: " + value);
+
     return value;
 }
 
 let nombre = "Test";
 let contrasenia = "incorrecta";
-console.log("\n------------------------------\nnameLogIn Call:")
-console.log("Name: " + nombre + "\nContrasenia: " + contrasenia);
+
+console.log("\n------------------------------");
+console.log("nameLogIn Call:");
+console.log("Name: " + nombre);
+console.log("Contrasenia: " + contrasenia);
+
 let logIn;
 nameLogIn(nombre, contrasenia).then((value) => {
     logIn = value;
+    console.log("------------------------------");
+    console.log("nameLogIn Promise:");
+    console.log("logIn: ");
+    console.log(logIn);
 });
+
 console.log("LogIn?: " + logIn);
 console.table(logIn);
