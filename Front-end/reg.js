017b7daea570ec2833c33017ffbc0cf4a6d70ef8
Marco Canton <67104790+MarcoCanton1@users.noreply.github.com>
2022-08-29 12:01:27 -0300

    Pagina register
    diseño basico de la pagina y un poco de javascript

--- Front-end/reg.js
+++ Front-end/reg.js
@@ -1,11 +1,9 @@
 document.addEventListener("DOMContentLoaded", () => {
+    const toggle = document.getElementById("");
     const logo = document.getElementById("logo");
+    const registrarse = document.getElementsByClassName("Register");
     const imgI = document.getElementById("imgI");
     const imgD = document.getElementById("imgD");
-
-    logo.addEventListener("click", () => {
-        window.location.replace("index.html");
-    });
 });
 
 imgI.addEventListener("click", () => {
@@ -15,3 +13,11 @@ imgI.addEventListener("click", () => {
 imgD.addEventListener("click", () => {
     window.location.replace("https://campus.ort.edu.ar/secundaria/belgrano/tic");
 });
+
+logo.addEventListener("click", () => {
+    window.location.replace("index.html");
+});
+
+function cambiar() {
+    registrarse.style.display = "flex";
+}
