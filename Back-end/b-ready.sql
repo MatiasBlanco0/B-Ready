5c9d90240c894199097be1e9b7b056c0b34904ae
MatiasBlanco0 <101400282+MatiasBlanco0@users.noreply.github.com>
2022-08-30 09:41:20 -0300

    fix(b-ready.sql): updated the file

--- Back-end/b-ready.sql
+++ Back-end/b-ready.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: localhost
--- Tiempo de generación: 22-08-2022 a las 12:54:41
+-- Tiempo de generación: 30-08-2022 a las 12:40:20
 -- Versión del servidor: 5.7.17-log
 -- Versión de PHP: 5.6.30
 
@@ -23,31 +23,6 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `materia`
---
-
-CREATE TABLE `materia` (
-  `ID` int(11) NOT NULL,
-  `Nombre` varchar(40) NOT NULL,
-  `NotaTotal` int(11) NOT NULL,
-  `CantTareasHechas` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `relacion materia/usuario`
---
-
-CREATE TABLE `relacion materia/usuario` (
-  `ID` int(11) NOT NULL,
-  `Email` varchar(40) NOT NULL,
-  `Materia` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- --------------------------------------------------------
-
---
 -- Estructura de tabla para la tabla `relacion usuario/tarea`
 --
 
@@ -56,6 +31,15 @@ CREATE TABLE `relacion usuario/tarea` (
   `Email` varchar(40) NOT NULL,
   `Tarea` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Volcado de datos para la tabla `relacion usuario/tarea`
+--
+
+INSERT INTO `relacion usuario/tarea` (`ID`, `Email`, `Tarea`) VALUES
+(1, 'test@test.test', 1),
+(2, 'hola@gmail.com', 1),
+(3, 'juancito@gmail.com', 1);
 
 -- --------------------------------------------------------
 
@@ -69,10 +53,17 @@ CREATE TABLE `tarea` (
   `Descripcion` varchar(200) NOT NULL,
   `CantEj` int(11) NOT NULL,
   `CantEjHechos` int(11) NOT NULL,
-  `Materia` int(11) NOT NULL,
+  `Materia` varchar(60) NOT NULL,
   `FechaEntrega` date NOT NULL,
   `Dificultad` tinyint(4) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Volcado de datos para la tabla `tarea`
+--
+
+INSERT INTO `tarea` (`ID`, `Nombre`, `Descripcion`, `CantEj`, `CantEjHechos`, `Materia`, `FechaEntrega`, `Dificultad`) VALUES
+(1, 'Proyecto', 'Proyecto B-ready B)', 10, 2, 'TIC', '2022-11-20', 127);
 
 -- --------------------------------------------------------
 
@@ -83,7 +74,7 @@ CREATE TABLE `tarea` (
 CREATE TABLE `usuario` (
   `Nombre` varchar(20) NOT NULL,
   `Email` varchar(40) NOT NULL,
-  `Contrasenia` varchar(25) NOT NULL
+  `Contrasenia` varchar(64) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
@@ -91,23 +82,13 @@ CREATE TABLE `usuario` (
 --
 
 INSERT INTO `usuario` (`Nombre`, `Email`, `Contrasenia`) VALUES
+('Hola', 'hola@gmail.com', 'hola123'),
+('Juan', 'juancito@gmail.com', '8d969eef6ecad3c29a3a629280e686cf0c3f5d5a86aff3ca12020c923adc6c92'),
 ('Test', 'Test@test.test', 'incorrecta');
 
 --
 -- Índices para tablas volcadas
 --
-
---
--- Indices de la tabla `materia`
---
-ALTER TABLE `materia`
-  ADD PRIMARY KEY (`ID`);
-
---
--- Indices de la tabla `relacion materia/usuario`
---
-ALTER TABLE `relacion materia/usuario`
-  ADD PRIMARY KEY (`ID`);
 
 --
 -- Indices de la tabla `relacion usuario/tarea`
@@ -132,20 +113,15 @@ ALTER TABLE `usuario`
 --
 
 --
--- AUTO_INCREMENT de la tabla `materia`
---
-ALTER TABLE `materia`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
---
 -- AUTO_INCREMENT de la tabla `relacion usuario/tarea`
 --
 ALTER TABLE `relacion usuario/tarea`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 --
 -- AUTO_INCREMENT de la tabla `tarea`
 --
 ALTER TABLE `tarea`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
