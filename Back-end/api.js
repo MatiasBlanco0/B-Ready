b7c185ddbebfd8c2eb230180ebe3f9448910103c
MatiasBlanco0 <101400282+MatiasBlanco0@users.noreply.github.com>
2022-08-30 09:22:29 -0300

    fix(getAssignmentInfo): changed the query
    the query had unnecessary columns

--- Back-end/api.js
+++ Back-end/api.js
@@ -100,10 +100,9 @@ async function getAssignments(userEmail){
 
 async function getAssignmentInfo(id) {
     try {
-        let sql = "SELECT tarea.id, tarea.nombre, tarea.descripcion, tarea.cantej, \
-        tarea.cantejhechos, tarea.materia, tarea.fechaentrega, tarea.dificultad, \
-        `relacion usuario/tarea`.email FROM tarea INNER JOIN `relacion usuario/tarea` \
-        ON tarea.id = `relacion usuario/tarea`.tarea WHERE tarea.id = ?";
+        let sql = "SELECT tarea.descripcion, `relacion usuario/tarea`.email \
+        FROM tarea INNER JOIN `relacion usuario/tarea` ON tarea.id = \
+        `relacion usuario/tarea`.tarea WHERE tarea.id = ?";
         return await sqlQuery(sql, [id]);
     } catch(err) {
         return err;
