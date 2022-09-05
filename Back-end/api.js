900bf3334f50de35391fa09154bf06b15093d72a
MatiasBlanco0 <101400282+MatiasBlanco0@users.noreply.github.com>
2022-09-05 08:59:14 -0300

    fix(api): get to post, response sends json

--- Back-end/api.js
+++ Back-end/api.js
@@ -8,10 +8,10 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || 8080;
 
-app.get('/getAssignments', (req, res) => {
+app.post('/getAssignments', (req, res) => {
     console.log("Recibi una request GET en /getAssignments");
     getAssignments(req.params['email'])
-    .then(result => res.send(result));
+    .then(result => res.json(result));
 });
 
 app.listen(port, () => console.log('Server started at http://localhost:' + port));
