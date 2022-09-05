67f11b2b738139f2c4aff1fa879449af20855c85
MatiasBlanco0 <101400282+MatiasBlanco0@users.noreply.github.com>
2022-09-05 08:59:26 -0300

    Testing of api

--- Back-end/test.js
+++ Back-end/test.js
@@ -1,8 +1,8 @@
 fetch("http://localhost:8080/getAssignments", {
-    method: "get",
-    body: new URLSearchParams({
-        "email": "test@test.test"
-    })
+    method: "post",
+    headers: {"Content Type": "application/json"},
+    body: JSON.stringify({"email": "test@test.test"})
 })
-.then(response => response.json())
-.then(data => console.log(data))
+.then(response => console.table(response.json()))
+.catch(err => console.log(err))
+.then(data => console.table(data))
