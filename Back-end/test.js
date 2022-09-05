8b5655d6143f3db29c6672b003b259f5324aa29e
MatiasBlanco0 <101400282+MatiasBlanco0@users.noreply.github.com>
2022-09-05 10:42:56 -0300

    Testing

--- Back-end/test.js
+++ Back-end/test.js
@@ -5,12 +5,10 @@ document.addEventListener("DOMContentLoaded", () =>{
 
     fetch("http://localhost:8080/getAssignments", {
         method: "post",
-        headers: {
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify({"email": "test@test.test"})
+        body: {"email": "test@test.test"}
     })
-    .then(response => response.innerHTML = response.json())
-    .catch(err => error.innerHTML = err)
-    .then(data => result.innerHTML = data)
+    .then(response => 
+        console.log(response)
+        )
+    .catch(err => console.log(err))
 });
