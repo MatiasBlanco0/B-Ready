d9b422d59f90a83e7379a86867ee55002e331c21
Marco Canton <67104790+MarcoCanton1@users.noreply.github.com>
2022-09-07 14:19:28 -0300

    fix(calendario): arregle el calendario en su mayoría :trollface:

--- Front-end/calendario.js
+++ Front-end/calendario.js
@@ -1,17 +1,53 @@
 const fecha = new Date();
-//fecha.setMonth(5);
-const meses = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"];
-const diasMes = document.querySelector(".dias");
-const ultimoDia = new Date(fecha.getFullYear(), fecha.getMonth(), + 1, 0);
 
-let dias = "";
+const renderCalendario = () => {
+    const meses = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"];
+    const diasMes = document.querySelector(".dias");
+    const ultimoDia = new Date(fecha.getFullYear(), fecha.getMonth() + 1, 0).getDate();
+    const primerDiaIndex = fecha.getDay();
+    const ultimoDiaPrevio = new Date(fecha.getFullYear(), fecha.getMonth(), 0).getDate();
+    const ultimoDiaIndex = new Date(fecha.getFullYear(), fecha.getMonth() + 1, 0).getDay();
 
-document.querySelector(".fecha h1").innerHTML = meses[fecha.getMonth()];
-document.querySelector(".fecha p").innerHTML = fecha.toLocaleDateString("es-US", { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' });
+    let dias = "";
+    const proximosDias = 7 - ultimoDiaIndex;
 
-for(let i = 1; i <= 31; i++){
-    dias += `<div>${i}</div>`;
-    diasMes.innerHTML = dias; //esto cambia el look, te hace sacar los dias en el html
-}
+    document.querySelector(".fecha h1").innerHTML = meses[fecha.getMonth()];
+    document.querySelector(".fecha p").innerHTML = fecha.toLocaleDateString("es-US", { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' });
 
-console.log(ultimoDia);
+    for (let x = primerDiaIndex; x > 0; x--) {
+        dias += `<div class="fecha-prev">${ultimoDiaPrevio - x + 1}</div>`;
+    }
+
+    for (let i = 1; i <= ultimoDia; i++) {
+
+        if (i === new Date().getDate() && fecha.getMonth() === new Date().getMonth()) {
+            dias += `<div class="hoy">${i}</div>`;
+        }
+        else {
+            dias += `<div>${i}</div>`;
+        }
+    }
+
+    for (let j = 1; j <= proximosDias; j++) {
+        dias += `<div class="fecha-next">${j}</div>`;
+        diasMes.innerHTML = dias;
+    }
+};
+
+document.querySelector(".prev").addEventListener("click", () => {
+    fecha.setMonth(fecha.getMonth() - 1);
+    renderCalendario();
+});
+
+document.querySelector(".next").addEventListener("click", () => {
+    fecha.setMonth(fecha.getMonth() + 1);
+    renderCalendario();
+});
+
+renderCalendario();
+
+document.querySelector(".dias").addEventListener("click", (evento) => {
+    if(!evento.target.classList.contains("fecha-next") && !evento.target.classList.contains("fecha-prev")){
+        //abrir cuadro de tarea
+    }
+});
