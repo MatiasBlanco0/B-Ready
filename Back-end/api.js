61f43108c9e112f78a79c6dc6787250d5d72d987
MatiasBlanco0 <101400282+MatiasBlanco0@users.noreply.github.com>
2022-09-16 08:53:30 -0300

    feat(api): added authenticateToken function

--- Back-end/api.js
+++ Back-end/api.js
@@ -31,6 +31,21 @@ function validateBody(req, res, next) {
     }
 }
 
+function authenticateToken(req, res, next) {
+    const authHeader = req.headers['autorizacion'];
+    const token = authHeader && authHeader.split(" ")[1];
+
+    if (token == null) {
+        return res.sendStatus(401);
+    }
+
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+        if (err) return res.sendStatus(403);
+
+        req.user = user;
+        next();
+    });
+}
 
 app.post('/login', validateBody, (req, res) => {
     console.log("\nRecibi una request POST en /login");
