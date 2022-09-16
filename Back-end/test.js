a246b4038d5c0efe803f2ec7a62cf857a821abc4
MatiasBlanco0 <101400282+MatiasBlanco0@users.noreply.github.com>
2022-09-16 08:57:54 -0300

    Testing

--- Back-end/test.js
+++ Back-end/test.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", () => {
-    fetch("http://localhost:8080/addUser", {
+    fetch("http://localhost:8080/login", {
         method: "post",
         mode: "cors",
         headers: {
@@ -9,17 +9,8 @@ document.addEventListener("DOMContentLoaded", () => {
         // nombre: "Juancito", email: "juan@gmail.com", contrasenia: "Juanprogamer10"
         // nombre: "Nahuel_P", email: "nahuelPer@gmail.com", contrasenia: "Nahuelsinho7"
         body: JSON.stringify({
-            nombre: "Pegarme un tiro",
-            descripcion: "Pegarme un tiro, eso",
-            ejercicios: 10,
-            ejerciciosHechos: 0,
-            materia: "Vida Real",
-            fecha: new Date(),
-            difficultad: 255,
-            email: "nahuelPer@gmail.com",
-            duenio: "juan@gmail.com",
-            contrasenia: "Juanprogamer10",
-            id: 7
+            email: "juan@gmail.com",
+            contrasenia: "Juanprogamer10"
         })
     })
         .then(response => {
