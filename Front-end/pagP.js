be1ca8518d29e521e0ba5c848de27d3ccac9d8fb
MatiasBlanco0 <101400282+MatiasBlanco0@users.noreply.github.com>
2022-10-27 13:39:14 -0300

    feat(index): changed for url params

--- Front-end/pagP.js
+++ Front-end/pagP.js
@@ -5,67 +5,9 @@ document.addEventListener("DOMContentLoaded", () => {
     let abierto = false;
     const Menu = document.getElementById("Menu");
 
-    imgI.addEventListener("click", () => {
-        window.location.replace("https://campus.ort.edu.ar/");
-    });
-
-    imgD.addEventListener("click", () => {
-        window.location.replace("https://campus.ort.edu.ar/secundaria/belgrano/tic");
-    });
-
-    document.querySelector("#MenuIco").addEventListener("click", () => {
-        if (!abierto) {
-            Menu.style.top = "100px";
-            Menu.style.transition = "1s";
-            document.body.style.overflow = "hidden";
-            document.getElementById("pagina").style.filter = "blur(2px)";
-            abierto = true;
-        }
-        else {
-            Menu.style.top = "-100px";
-            Menu.style.transition = "1s";
-            document.body.style.overflow = "scroll";
-            abierto = false;
-            document.getElementById("pagina").style.filter = "none";
-        }
-    });
-
-    document.getElementById("pagina").addEventListener("click", () => {
-        if (abierto) {
-            Menu.style.top = "-100px";
-            Menu.style.transition = "1s";
-            document.body.style.overflow = "scroll";
-            abierto = false;
-            document.getElementById("pagina").style.filter = "none";
-        }
-    });
-
-    function BuscarCookie(nombre) {
-        let cookie = document.cookie;
-        let prefijo = nombre + "=";
-        let inicio = cookie.indexOf("; " + prefijo);
-
-        //check si el indexOf no encuentra nada
-        if (inicio == -1) {
-            inicio = cookie.indexOf(prefijo);
-            //si no es igual a cero
-            if (inicio != 0) {
-                return null;
-            }
-        }
-        else {
-            inicio += 2;
-            let fin = cookie.indexOf(";", inicio);
-            if (fin == -1) {
-                fin = cookie.length;
-            }
-        }
-        //crea un substring desde que encuentra la cookie que se busca y sigue hasta el final
-        return decodeURI(cookie.substring(inicio + prefijo.length, fin)); //fijar si la cookie viene codificada y es necesario el decodeURI
-    }
-
-    let accessToken = BuscarCookie("BReadyAccessToken");
-    let refreshToken = BuscarCookie("BReadyRefreshToken");
+    const urlParams = new URLSearchParams(window.location.search);
+    let accessToken = urlParams.get("at");
+    let refreshToken = urlParams.get("rt");
 
     if (accessToken != null) {
         //cambios cuando uno se logea
@@ -107,10 +49,14 @@ document.addEventListener("DOMContentLoaded", () => {
                         document.querySelector(".log").style.display = "block";
                         document.querySelector(".log1").style.display = "block";
                         document.querySelector(".deslog").style.display = "none";
+                        return response.json();
                     }
                     else { //Error interno
                         location.reload();
                     }
+                })
+                .then(data => {
+                    window.location.replace(`index.html?at=${data.accessToken}&rt=${refreshToken}`);
                 })
                 .catch(err => {
                     console.log("Error: ");
@@ -118,5 +64,67 @@ document.addEventListener("DOMContentLoaded", () => {
                 });
         }
     }
+
+    imgI.addEventListener("click", () => {
+        window.location.replace("https://campus.ort.edu.ar/");
+    });
+
+    imgD.addEventListener("click", () => {
+        window.location.replace("https://campus.ort.edu.ar/secundaria/belgrano/tic");
+    });
+
+    document.querySelector("#MenuIco").addEventListener("click", () => {
+        if (!abierto) {
+            Menu.style.top = "100px";
+            Menu.style.transition = "1s";
+            document.body.style.overflow = "hidden";
+            document.getElementById("pagina").style.filter = "blur(2px)";
+            abierto = true;
+        }
+        else {
+            Menu.style.top = "-100px";
+            Menu.style.transition = "1s";
+            document.body.style.overflow = "scroll";
+            abierto = false;
+            document.getElementById("pagina").style.filter = "none";
+        }
+    });
+
+    document.getElementById("pagina").addEventListener("click", () => {
+        if (abierto) {
+            Menu.style.top = "-100px";
+            Menu.style.transition = "1s";
+            document.body.style.overflow = "scroll";
+            abierto = false;
+            document.getElementById("pagina").style.filter = "none";
+        }
+    });
+
+    /* function BuscarCookie(nombre) {
+        let cookie = document.cookie;
+        let prefijo = nombre + "=";
+        let inicio = cookie.indexOf("; " + prefijo);
+
+        //check si el indexOf no encuentra nada
+        if (inicio == -1) {
+            inicio = cookie.indexOf(prefijo);
+            //si no es igual a cero
+            if (inicio != 0) {
+                return null;
+            }
+        }
+        else {
+            inicio += 2;
+            let fin = cookie.indexOf(";", inicio);
+            if (fin == -1) {
+                fin = cookie.length;
+            }
+        }
+        //crea un substring desde que encuentra la cookie que se busca y sigue hasta el final
+        return decodeURI(cookie.substring(inicio + prefijo.length, fin)); //fijar si la cookie viene codificada y es necesario el decodeURI
+    }
+
+    let accessToken = BuscarCookie("BReadyAccessToken");
+    let refreshToken = BuscarCookie("BReadyRefreshToken"); */
 });
 
